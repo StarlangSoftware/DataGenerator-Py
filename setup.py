c2eebad911ca43eb4cf05fdd04a1cc13ee57fe6a
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-02-10 21:10:36 +0300

    Updated DataGenerator repository translation.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='NlpToolkit-DataGenerator',
+    version='1.0.0',
+    packages=['DataGenerator', 'DataGenerator.Attribute', 'DataGenerator.CorpusGenerator',
+              'DataGenerator.DatasetGenerator', 'DataGenerator.InstanceGenerator'],
+    url='https://github.com/olcaytaner/DataGenerator-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Classification dataset generator library for high level Nlp tasks',
+    install_requires=['NlpToolkit-AnnotatedSentence', 'NlpToolkit-AnnotatedTree', 'NlpToolkit-Classification',
+                      'NlpToolkit-MorphologicalDisambiguation']
+)
