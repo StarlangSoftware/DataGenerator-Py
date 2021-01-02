5a7949a379098764cb63bfbc18574dfcadd7192a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:31:21 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='1.0.3',
     packages=['DataGenerator', 'DataGenerator.Attribute', 'DataGenerator.CorpusGenerator',
               'DataGenerator.DatasetGenerator', 'DataGenerator.InstanceGenerator'],
-    url='https://github.com/olcaytaner/DataGenerator-Py',
+    url='https://github.com/StarlangSoftware/DataGenerator-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Classification dataset generator library for high level Nlp tasks',
     install_requires=['NlpToolkit-AnnotatedSentence', 'NlpToolkit-AnnotatedTree', 'NlpToolkit-Classification',
                       'NlpToolkit-MorphologicalDisambiguation']
