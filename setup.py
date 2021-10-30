e0fd2a5ddcaef38c0a895d49039a2774483a2a42
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 20:15:54 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='NlpToolkit-DataGenerator',
-    version='1.0.3',
+    version='1.0.4',
     packages=['DataGenerator', 'DataGenerator.Attribute', 'DataGenerator.CorpusGenerator',
               'DataGenerator.DatasetGenerator', 'DataGenerator.InstanceGenerator'],
     url='https://github.com/StarlangSoftware/DataGenerator-Py',
@@ -11,5 +15,7 @@ setup(
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Classification dataset generator library for high level Nlp tasks',
     install_requires=['NlpToolkit-AnnotatedSentence', 'NlpToolkit-AnnotatedTree', 'NlpToolkit-Classification',
-                      'NlpToolkit-MorphologicalDisambiguation']
+                      'NlpToolkit-MorphologicalDisambiguation'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
